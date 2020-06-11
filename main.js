44d0f4d37491649a61980ffe7ee804ced2a17c9a
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-06-11 16:23:27 -0700

    lesson_06_advanced

--- main.js
+++ main.js
@@ -6,47 +6,44 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function guessNumber(message, amountOfAttempts) {
-  let number = 20;
-
-  userNumber = prompt("Guess number from 1 to 100: ");
+function guessNumber(message, amountOfAttempts, number) {
+  console.log("number to guess: " + number);
 
   function playAgain(msg) {
     let game = confirm(msg);
-    console.log(game);
     if (!game) {
       alert("ok, thanks!");
       return;
     } else {
-      return guessNumber("Lets play again", 10);
+      return guessNumber("Lets play new game", 10, Math.floor(Math.random() * 101));
     }
   }
-
+  
   if (amountOfAttempts === 0) {
-    return playAgain("no attempts left, game is over, do you want to play more?");
+    return playAgain("no attempts left, do you want to play again?");
   } else {
     amountOfAttempts = amountOfAttempts;
   }
   if (message && amountOfAttempts !== 0) {
-    alert(message + (amountOfAttempts ? ", amount of attempts left is " + amountOfAttempts : ""));
+    alert(message + (amountOfAttempts ? "amount of attempts left is " + amountOfAttempts : ""));
   }
+
+  userNumber = prompt("Guess number from 1 to 100: ");
 
   function checkNumber() {
     if (userNumber === null) {
       alert("sorry, you decided to leave, thanks for the game!");
       return;
     } else if (!isNumber(userNumber)) {
-      return guessNumber("enter number ", amountOfAttempts);
+      return guessNumber("enter number ", amountOfAttempts, number);
     } else if (number > userNumber) {
       return guessNumber(
-        "the number is higher than you guessed, try again ", --amountOfAttempts
-      );
+        "the number is higher than you guessed, try again, ", --amountOfAttempts, number);
     } else if (number < userNumber) {
       return guessNumber(
-        "the number is less than you guessed, try again ", --amountOfAttempts
-      );
+        "the number is less than you guessed, try again, ", --amountOfAttempts, number);
     } else {
-      return playAgain("correct! Do you want to play again?");
+      return playAgain("CORRECT! Do you want to play again?");
     }
   }
 
@@ -54,5 +51,5 @@ function guessNumber(message, amountOfAttempts) {
   return checkNumber();
 }
 
-const play = guessNumber("", 10);
+const play = guessNumber("Lets play ", 10, Math.floor(Math.random() * 101));
 console.log(play);
